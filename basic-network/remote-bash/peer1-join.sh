2fb16425efe0918533b152a8eb0ae324f4dc09f3
Thanh Phu <git@thanhphu.net>
2020-01-06 21:35:47 +0900

    Complete script

--- basic-network/remote-bash/peer1-join.sh
+++ basic-network/remote-bash/peer1-join.sh
@@ -1,5 +1,8 @@
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp
+~/fabric-samples/bin/peer channel create -o orderer0:7050 -c mychannel -f /home/medium/fabric-workdir/configtx/channel.tx
 
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1 peer channel create -o orderer0:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer1 to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1 peer channel join -b mychannel.block
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_MSPCONFIGPATH=/home/medium/fabric-workdir/msp/users/Admin@org1.example.com/msp
+~/fabric-samples/bin/peer channel join -b mychannel.block
--- basic-network/start-remote.sh
+++ basic-network/start-remote.sh
@@ -26,7 +26,12 @@ ssh fabric-ca 'bash -s' < ./remote-bash/peer1-init.sh
 scp -r ./crypto-config/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/msp peer2:/home/medium/fabric-workdir/msp/peer
 scp -r ./crypto-config/peerOrganizations/org1.example.com/users peer2:/home/medium/fabric-workdir/msp/users
 scp -r ./config peer2:/home/medium/fabric-workdir/configtx
-ssh fabric-ca 'bash -s' < ./remote-bash/peer2-init.sh
+ssh fabric-ca 'bash -s' < (cat ./remote-bash/peer1-init.sh | sed -e 's|peer1|peer2|')
+
+scp -r ./crypto-config/peerOrganizations/org1.example.com/peers/peer3.org1.example.com/msp peer3:/home/medium/fabric-workdir/msp/peer
+scp -r ./crypto-config/peerOrganizations/org1.example.com/users peer3:/home/medium/fabric-workdir/msp/users
+scp -r ./config peer3:/home/medium/fabric-workdir/configtx
+ssh fabric-ca 'bash -s' < (cat ./remote-bash/peer1-init.sh | sed -e 's|peer1|peer3|')
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -35,5 +40,5 @@ export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 ssh fabric-ca 'bash -s' < ./remote-bash/peer1-join.sh
-ssh fabric-ca 'bash -s' < ./remote-bash/peer2-join.sh
-ssh fabric-ca 'bash -s' < ./remote-bash/peer3-join.sh
+ssh fabric-ca 'bash -s' < (cat ./remote-bash/peer2-join.sh | sed -e 's|peer1|peer2|')
+ssh fabric-ca 'bash -s' < (cat ./remote-bash/peer2-join.sh | sed -e 's|peer1|peer3|')
